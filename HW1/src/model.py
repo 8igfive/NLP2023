8262da85dd11cdaf9a80531af03a69af0d8508ba
8igfive <limx0204@buaa.edu.cn>
2023-03-16 04:11:30 +0000

    Add&Update: train.py, model.py

--- HW1/src/model.py
+++ HW1/src/model.py
@@ -23,7 +23,7 @@ class LanguageModel(nn.Module):
             unitoken = corpus[i]
             self.unigram_count[unitoken] = self.unigram_count.get(unitoken, 0) + 1
             if i + 1 < len(corpus):
-                bitoken = corpus[i: i + 2]
+                bitoken = tuple(corpus[i: i + 2])
                 self.bigram_count[bitoken] = self.bigram_count.get(bitoken, 0) + 1
                 if bitoken[0] not in self.bigram_freq:
                     self.bigram_freq[bitoken[0]] = dict()
@@ -39,34 +39,71 @@ class LanguageModel(nn.Module):
         self.bigram_freq_sum = {key: sum(value.values()) for key, value in self.bigram_freq.items()}
         self.trigram_freq_sum = {key: sum(value.values()) for key, value in self.trigram_freq.items()}
 
-    def initial_p(self, bitoken: Tuple[str, str]):
+    def initial_p(self, bitoken: Tuple[str, str]) -> float:
         return self.bigram_count.get(bitoken, 0) / self.bigram_count_sum
 
-    def forward(self, tritokens: List[Tuple[str, str, str]]):
+    def f_3(self, tritokens: List[Tuple[str, str, str]]) -> torch.Tensor:
+        f = torch.empty(len(tritokens), dtype=torch.float32)
+        for i, tritoken in enumerate(tritokens):
+            if tritoken[:2] not in self.trigram_freq or \
+                tritoken[2] not in self.trigram_freq[tritoken[:2]]:
+                f[i] = 1 / self.bigram_count_sum
+            else:
+                f[i] = self.trigram_freq[tritoken[:2]].get(tritoken[2], 0) / self.trigram_freq_sum[tritoken[:2]]
+        return f
+
+    def forward(self, tritokens: List[Tuple[str, str, str]]) -> torch.Tensor:
         f_0 = torch.stack([self.f0 for _ in range(len(tritokens))])
-        f_1 = torch.empty(len(tritoken), dtype=torch.float32)
-        f_2 = torch.empty(len(tritoken), dtype=torch.float32)
-        f_3 = torch.empty(len(tritoken), dtype=torch.float32)
-        counts = torch.empty(len(tritoken), 3, dtype=torch.float32)
+        f_1 = torch.empty(len(tritokens), dtype=torch.float32)
+        f_2 = torch.empty(len(tritokens), dtype=torch.float32)
+        f_3 = torch.empty(len(tritokens), dtype=torch.float32)
+        counts = torch.empty(len(tritokens), 3, dtype=torch.float32)
         counts[:, 2] = 1
         for i, tritoken in enumerate(tritokens):
-            counts[i][0] = self.bigram_count.get(tritoken[:2], 0) / self.bigram_count_sum
-            counts[i][1] = self.unigram_count.get(tritoken[0], 0) / self.unigram_count_sum
+            counts[i][0] = self.bigram_count.get(tritoken[:2], 1)
+            counts[i][1] = self.unigram_count.get(tritoken[0], 1)
             
-            f_1[i] = self.unigram_count.get(tritoken[2], 0) / self.unigram_count_sum
+            f_1[i] = self.unigram_count.get(tritoken[2], 1) / self.unigram_count_sum
             
-            if tritoken[1] not in self.bigram_freq:
-                f_2[i] = 0
+            if tritoken[1] not in self.bigram_freq or \
+                tritoken[2] not in self.bigram_freq[tritoken[1]]:
+                f_2[i] = 1 / self.bigram_count_sum
             else:
-                f_2[i] = self.bigram_freq[tritoken[1]].get(tritoken[2], 0) / self.bigram_freq_sum[tritoken[1]]
+                f_2[i] = self.bigram_freq[tritoken[1]][tritoken[2]] / self.bigram_freq_sum[tritoken[1]]
             
-            if tritoken[:2] not in self.trigram_freq:
-                f_3[i] = 0
+            if tritoken[:2] not in self.trigram_freq or \
+                tritoken[2] not in self.trigram_freq[tritoken[:2]]:
+                f_3[i] = 1 / self.bigram_count_sum
             else:
-                f_3[i] = self.trigram_freq[tritoken[:2]].get(tritoken[2], 0) / self.trigram_freq_sum[tritoken[:2]]
+                f_3[i] = self.trigram_freq[tritoken[:2]][tritoken[2]] / self.trigram_freq_sum[tritoken[:2]]
         lambdas = [counts @ lambda_param for lambda_param in self.lambda_params] # List[tensor of shape(bs)]
         fs = torch.stack([f_0, f_1, f_2, f_3], dim=-1)
         lambdas =  torch.softmax(torch.stack(lambdas, dim=-1))
         lm_p = (fs * lambdas).sum(dim=-1)
         
         return lm_p
+    
+    def save_model(self, save_path: str):
+        torch.save({
+            "unigram_count": self.unigram_count,
+            "unigram_count_sum": self.unigram_count_sum,
+            "bigram_count": self.bigram_count,
+            "bigram_count_sum": self.bigram_count_sum,
+            "bigram_freq": self.bigram_freq,
+            "bigram_freq_sum": self.bigram_freq_sum,
+            "trigram_freq": self.trigram_freq,
+            "trigram_freq_sum": self.trigram_freq_sum,
+            "state_dict": self.state_dict()
+        }, save_path)
+    
+    def load_model(self, model_path: str):
+        ckpt = torch.load(model_path)
+        self.unigram_count = ckpt["unigram_count"]
+        self.unigram_count_sum = ckpt["unigram_count_sum"]
+        self.bigram_count = ckpt["bigram_count"]
+        self.bigram_count_sum = ckpt["bigram_count_sum"]
+        self.bigram_freq = ckpt["bigram_freq"]
+        self.bigram_freq_sum = ckpt["bigram_freq_sum"]
+        self.trigram_freq = ckpt["trigram_freq"]
+        self.trigram_freq_sum = ckpt["trigram_freq_sum"]
+        self.load_state_dict(ckpt["state_dict"])
---
+++ HW1/src/train.py
@@ -0,0 +1,58 @@
+import os
+import time
+import torch
+import random
+from typing import Tuple, List
+from torch.optim import Adam
+from torch import nn
+from model import LanguageModel
+
+class Trainer:
+    
+    def __init__(self, model_train: LanguageModel, model_target: LanguageModel, 
+                 train_corpus: List[str], lr: float = 1e-3):
+        self.model_train = model_train
+        self.model_target = model_target
+        self.train_corpus = train_corpus
+
+        self.optimizer = Adam(model_train.parameters(), lr=lr)
+
+    def train(self, epochs: int, batch_size: int, pring_interval: int):
+        self.model_train.train()
+        self.model_target.eval()
+        data_indices = list(range(len(self.train_corpus) - 2))
+        loss_fn = nn.MSELoss()
+        loss_sum = 0
+        for epoch in range(epochs):
+            print(f"\nEpoch {epoch} starts.\n")
+            random.shuffle(data_indices)
+            for index_base in range(0, len(data_indices), batch_size):
+                step = index_base // batch_size
+                tritokens = []
+                for batch_i in range(batch_size):
+                    data_index = data_indices[index_base + batch_i]
+                    tritokens.append(tuple(self.train_corpus[data_index: data_index + 3]))
+                train_p = self.model_train(tritokens)
+                target_p = self.model_target.f_3(tritokens)
+                loss = loss_fn(train_p, target_p)
+
+                self.optimizer.zero_grad()
+                loss.backward()
+                self.optimizer.step()
+
+                loss_sum += loss.item()
+                if step % pring_interval == 0:
+                    print(f"epoch: {epoch}, step: {step}, loss: {loss.item()}, avg_loss: {loss_sum / step}")
+        
+    def load_models(self, train_path: str, target_path: str):
+        self.model_train.load_model(train_path)
+        self.model_target.load_model(target_path)
+
+    def save_models(self):
+        dump_dir = r'../dump'
+        save_name_p = "{} {}.ckpt"
+        cur_time = time.asctime()
+        os.makedirs(dump_dir, exist_ok=True)
+        self.model_train.save_model(os.path.join(dump_dir, save_name_p.format(cur_time, "model_train")))
+        self.model_target.save_model(os.path.join(dump_dir, save_name_p.format(cur_time, "model_target")))
+
